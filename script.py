314787d962a7bc8e5d1b0455f0f9b264145977a6
Christopher <chris@theantichris.com>
2020-01-06 07:50:26 -0500

    performing the initial split of the list

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 def merge_sort(items):
     if len(items) <= 1:
         return items
+
+    middle_index = len(items) // 2
+    left_split = items[:middle_index]
+    right_split = items[middle_index:]
+
+    return middle_index, left_split, right_split
